0cd59126ecf7dd605e8618d29a140045de6b6566
rezaramadhani <rezarmd17@gmail.com>
2022-12-04 22:30:07 +0700

    membuat if

---
+++ src/test/java/ws/prak/postman/pws/c/prak/postman/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.prak.postman.pws.c.prak.postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
